eb54738997c2d1447b1402503e6eb4af6a5d7f81
Maciej Sobczak <sobczakm161@gmail.com>
2018-06-04 02:51:38 +0200

    basic version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TrendService } from './trend.service';
+import { map } from 'rxjs/operators';
 import { Chart } from 'chart.js';
 
 @Component({
@@ -9,16 +10,56 @@ import { Chart } from 'chart.js';
 })
 export class AppComponent {
   title = 'app';
+  public trends;
+  public timeSamples = [];
+  public values = [];
+  public chart;
 
   constructor(private _trend: TrendService) {
     this.NgOnInit();
-    console.log('thats it');
   }
 
   NgOnInit() {
+    this.getData();
+  }
+
+  private getData() {
     this._trend.dailyTrends()
-      .subscribe(res => {
-        console.log(res);
-      });
+      .subscribe(
+        data => {
+          this.trends = data;
+          this.trends.forEach(element => {
+            this.values.push(element.average);
+            this.timeSamples.push(element.time);
+          });
+          this.chart = new Chart('canvas', {
+            type: 'line',
+            data: {
+              labels: this.timeSamples,
+              datasets: [
+                {
+                  data: this.values,
+                  borderColor: '#3cba9f',
+                  fill: false
+                }
+              ]
+            },
+            options: {
+              legend: {
+                display: false
+              },
+              scales: {
+                xAxes: [{
+                  display: true
+                }],
+                yAxes: [{
+                  display: true
+                }],
+              }
+            }
+          });
+        },
+        err => { console.error(err); }
+      );
   }
 }
--- src/app/trend.service.ts
+++ src/app/trend.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrendService {
 
+  public ids = ['BTCUSD', 'LTCUSD', 'ETHUSD'];
   constructor(private _http: HttpClient) { }
 
   dailyTrends() {
-    return this._http.get('https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=daily&?format=json');
+    return this._http.get('https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=monthly&?format=json');
   }
 }
